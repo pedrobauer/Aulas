167d5a341d351f45d59c0e0485be69f6a1f738e7
Pedro Henrique Lopes Bauer <80714241+pedrobauer@users.noreply.github.com>
2022-09-14 20:27:40 -0300

    Arrey-While-For

---
+++ While-For.js
@@ -0,0 +1,65 @@
+/*paises = ["Brasil", "Polonia", "Argentina", "França", "Canada"];
+
+//WHILE
+let indice = 0
+while (indice < paises.length) {
+    console.log(paises[indice]);
+    indice++;
+}
+
+FOR
+for (let index = 0; index < paises.length; index++) {
+    console.log(paises[index]);
+}
+
+//Exercicio 3
+numeros = [1, 2, 3, 4, 5];
+
+let total = 0;
+for (let i = 0; i < numeros.length; i++) {
+    total = total + numeros[i];
+    //soma+=numeros[i]
+}
+console.log(total); 
+
+//For of
+const nomes = ['jose', 'joao', 'maria', 'joana', 'andre']
+for (let nome of nomes) {
+    console.log(nome)
+}
+
+
+//Percorrer uma string como se fosse um Arrey
+//Exercicio 4
+
+const palavra = 'abracadabra';
+let totalDeA = 0;
+for (let letra of palavra) {
+    if (letra == 'a') {
+        totalDeA += 1
+    }
+}
+console.log(totalDeA);
+
+//Exercicio 5
+
+let timer = 10;
+
+while (timer >= 0) {
+    console.log(timer);
+    timer--;
+}
+for (let timer = 10; timer >= 0; timer--) {
+    console.log(timer);
+    //soma+=numeros[i]
+}
+
+//Exercicio 6
+for (let i = 2; i <= 50; i += 2) {
+    console.log(i);
+}*/
+let num = 2;
+while (num <= 50) {
+    console.log(num);
+    num += 2
+}
---
+++ arrays.js
@@ -0,0 +1,21 @@
+paises = ["Brasil", "Polonia", "Argentina", "França", "Canada"];
+
+paises.push("China");
+
+console.log(paises);
+
+paises.pop();
+
+console.log(paises);
+
+paises.unshift("China");
+
+console.log(paises);
+
+paises.shift();
+
+console.log(paises);
+
+console.log(paises[paises.length - 1]);
+console.log(paises[1]);
+console.log(paises[2]);

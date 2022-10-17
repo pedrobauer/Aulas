9d70dfafd937762cf918bdbfcb75b6ca195f622e
Pedro Henrique Lopes Bauer <80714241+pedrobauer@users.noreply.github.com>
2022-10-17 17:05:57 -0300

    Atualização

---
+++ funçao.js
@@ -0,0 +1,85 @@
+// //Exercicio 3
+// function verificarMaiorIdade(pessoa) {
+//     if (pessoa.idade <= 21) {
+//         return "jovem";
+//     } else if (pessoa.idade < 65) {
+//         return "adulto(a)";
+//     } else {
+//         return "idoso(a)";
+//     }
+// }
+// function apresentar(pessoa) {
+//     console.log(`Ola! meu nome e ${pessoa.nome}, sou um ${verificarMaiorIdade(pessoa)} de ${pessoa.idade} anos, ${pessoa.altura}m de altura e sou ${pessoa.profissao}.`)
+// }
+// const pessoa = {
+//     nome: "Joao",
+//     idade: 12,
+//     altura: 1.4,
+//     profissao: "estudante"
+// }
+// apresentar(pessoa);
+
+// function verificarMaiorIdade(idade) {
+//     if (idade <= 21) {
+//         return "jovem";
+//     } else if (idade < 65) {
+//         return "adulto(a)";
+//     } else {
+//         return "idoso(a)";
+//     }
+// }
+// const pessoa = {
+//     nome: "jose",
+//     idade: 30,
+//     profissao: "professor"
+// }
+// const pessoa2 = {
+//     nome: "pedro",
+//     idade: 65,
+//     profissao: "medico"
+// }
+// const pessoa3 = {
+//     nome: "ana",
+//     idade: 19,
+//     profissao: "estudante"
+// }
+// console.log(verificarMaiorIdade(pessoa.idade));
+// console.log(verificarMaiorIdade(pessoa2.idade));
+// console.log(verificarMaiorIdade(pessoa3.idade));
+
+// function verificarMaiorIdade(idade) {
+//     if (idade <= 21) {
+//         return "jovem";
+//     } else if (idade <= 65) {
+//         return "adulto(a)";
+//     } else {
+//         return "idoso(a)";
+//     }
+// }
+// console.log(verificarMaiorIdade(70))
+
+// //Exercicio 1
+// const pessoa = {
+//     nome: "Joao",
+//     idade: 12,
+//     profissao: "estudante",
+//     altura: 1.4
+// }
+// function apresentar(nome, idade, profissao, altura) {
+//     console.log(`Ola! Meu nome e ${nome}, sou um jovem de ${idade}anos, ${altura}m de altura e sou ${profissao}.`)
+// }
+// apresentar(pessoa.nome, pessoa.idade, pessoa.profissao, pessoa.altura);
+
+// function cumprimentar(pessoa) {
+//     console.log(`ola, ${pessoa}`)
+// }
+// cumprimentar("jose");
+// cumprimentar("pedro");
+// cumprimentar("ana");
+// cumprimentar("ronaldo");
+
+
+// function imprimirOla() {
+//     console.log("ola")
+// }
+// imprimirOla();
